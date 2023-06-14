e4ba85f0b70b3e3ba1f82da90a1f712f908b9a38
HarshaRatnal <135690201+HarshaRatnal@users.noreply.github.com>
2023-06-13 23:59:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const path = require('path');
+const userRoutes = require('./server/routes/User');
+const postRoutes = require('./server/routes/Post');
+//console.log(process.env.dbURL);
+mongoose.connect(process.env.dbURL)
+  .then(console.log("DB Connected!!"))
+  .catch(error => console.log(error));
 
 app.use(express.json());
 
-app.use(express.static(__dirname + "/public" ));
-app.get("/", (req,res) => res.sendFile(path.joint(__dirname, '/public')))
+app.use(express.static(__dirname + "/public"));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public', 'index.html')));
 
-//CORS middleware
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");  
-  res.header("Access-Control-Allow-Headers"
-  , "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");  
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin,X-Requested-Width,Content-Type,Accept,Authorization");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS")
   next();
 });
 
-
+app.use('/User', userRoutes);
+app.use('/Post', postRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
